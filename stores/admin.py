4889c78a123126424d09aac4d4a907733736a0f0
hanodims <hend--mohammed@outlook.com>
2020-09-13 20:03:15 +0300

    Customizing Admin Page

--- stores/admin.py
+++ stores/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 from .models import Store
 
 # Register your models here.
-admin.site.register(Store)
+
+
+
+class StoreAdmin(admin.ModelAdmin):
+    fields = ['name', 'description']
+    list_display = (id, 'name', 'description')
+    list_filter = ['added']
+    search_fields = ['name', 'description']
+    list_editable = ( 'description', )
+    list_display_links = ('name',)
+
+
+admin.site.register(Store, StoreAdmin)
+
+
